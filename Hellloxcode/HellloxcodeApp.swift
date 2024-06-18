40b2105afdee67e5fe35e90c3df95440dbff4f62
DAKSHCTU11 <dakshctu11@gmail.com>
2024-06-18 12:14:00 +0530

    Initial Commit

---
+++ Hellloxcode/HellloxcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HellloxcodeApp.swift
+//  Hellloxcode
+//
+//  Created by Daksh Chaudhary on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HellloxcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
